5d0e92220234325a0e5ab39ba6c18fab3a7f11f5
SrankoMate <tothmate020@gmail.com>
2022-12-19 13:34:11 +0100

    Versenyzők class kész.

--- src/Versenyzok.java
+++ src/Versenyzok.java
@@ -1,2 +1,70 @@
+import java.sql.Time;
+
 public class Versenyzok {
+    private String rajtszam;
+    private String kategoria;
+    private String nev;
+    private String egyesulet;
+    private Time ido;
+
+    public Versenyzok() {
+    }
+
+    public Versenyzok(String rajtszam, String kategoria, String nev, String egyesulet, Time ido) {
+        this.rajtszam = rajtszam;
+        this.kategoria = kategoria;
+        this.nev = nev;
+        this.egyesulet = egyesulet;
+        this.ido = ido;
+    }
+
+    public Versenyzok(String csvSor) {
+        String[] ertekek = csvSor.split(";");
+
+        this.rajtszam = ertekek[0];
+        this.kategoria = ertekek[1];
+        this.nev=ertekek[2];
+        this.egyesulet=ertekek[3];
+        this.ido=Time.valueOf(ertekek[4]);
+    }
+
+    public String getRajtszam() {
+        return rajtszam;
+    }
+
+    public void setRajtszam(String rajtszam) {
+        this.rajtszam = rajtszam;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public String getEgyesulet() {
+        return egyesulet;
+    }
+
+    public void setEgyesulet(String egyesulet) {
+        this.egyesulet = egyesulet;
+    }
+
+    public Time getIdo() {
+        return ido;
+    }
+
+    public void setIdo(Time ido) {
+        this.ido = ido;
+    }
 }
